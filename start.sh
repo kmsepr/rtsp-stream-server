1b1b6a1814b3578b40abe8859dc411e2e0908b4f
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-15 12:53:21 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start the RTSP server
+./mediamtx &
+
+# Start FFmpeg stream
+ffmpeg -re -i "https://edge1.1internet.tv/dash-live2/streams/1tv-dvr/1tvdash.mpd" \
+  -map 0:v:3 -map 0:a:1 \
+  -acodec amr_wb -ar 16000 -ac 1 \
+  -vcodec h263 -vb 70k -r 15 \
+  -vf scale=176:144 \
+  -f rtsp rtsp://127.0.0.1:8554/live
